ea8085fafb3ef13a1ae1142370f9f18d678fc1fd
ayush-govindwar <swayush.govindwar@gmail.com>
2025-02-11 11:53:24 +0530

    feat: set up database connection and api endpoints

--- app.js
+++ app.js
@@ -1,7 +1,13 @@
 require('dotenv').config();
 require('express-async-errors');
 const express = require('express');
+const authRouter = require('./routes/auth')
+const jobsRouter = require('./routes/jobs')
 const app = express();
+
+//connectDB
+const connectDB = require('./db/connect')
+
 
 // error handler
 const notFoundMiddleware = require('./middleware/not-found');
@@ -11,17 +17,17 @@ app.use(express.json());
 // extra packages
 
 // routes
-app.get('/', (req, res) => {
-  res.send('jobs api');
-});
+app.use('/api/v1/auth', authRouter)
+//app.use('/api/v1/jobs', jobsRouter)
 
 app.use(notFoundMiddleware);
 app.use(errorHandlerMiddleware);
 
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 4000;
 
 const start = async () => {
   try {
+    await connectDB(process.env.MONGO_URI)
     app.listen(port, () =>
       console.log(`Server is listening on port ${port}...`)
     );
