5c0e3f73b55bb087820cdf9bad573f7a3878f261
ayush-govindwar <swayush.govindwar@gmail.com>
2025-02-11 17:47:44 +0530

    feat: set up pre middleware and instance methods

--- controllers/auth.js
+++ controllers/auth.js
@@ -2,6 +2,7 @@ const User = require('../models/User')
 const {StatusCodes} = require('http-status-codes')
 const bcrypt= require('bcryptjs')
 const jwt = require('jsonwebtoken')
+const { BadRequestError, UnauthenticatedError } = require('../errors')
 
 
 
@@ -10,13 +11,27 @@ const jwt = require('jsonwebtoken')
 
 const register = async(req,res)=>{
     const user = await User.create({...req.body}) //...spread operator 
-    const token = await jwt.sign({userId: user._id,name:user.name},'jwtSecret',
-        {expiresIn:'30d'})
+    const token = user.createJWT()
     res.status(StatusCodes.CREATED).json({user: {name: user.name},token})
 }
 
 const login = async(req,res)=>{
-    res.send("register login")
+    const { email, password } = req.body
+
+    if (!email || !password) {
+      throw new BadRequestError('Please provide email and password')
+    }
+    const user = await User.findOne({ email })
+    if (!user) {
+      throw new UnauthenticatedError('Invalid Credentials')
+    }
+    const isPasswordCorrect = await user.comparePassword(password)
+    if (!isPasswordCorrect) {
+      throw new UnauthenticatedError('Invalid Credentials')
+    }
+    // compare password
+    const token = user.createJWT()
+    res.status(StatusCodes.OK).json({ user: { name: user.name }, token })
 }
 
 module.exports={
